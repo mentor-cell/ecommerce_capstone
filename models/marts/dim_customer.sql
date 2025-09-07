95d071fb9169d77f2eccb921af04c9b18c665084
mentor@analyticsengineering.com <mentor@analyticsengineering.com>
2025-09-07 03:45:32 +0000

    dim changes

---
+++ models/marts/dim_customer.sql
@@ -0,0 +1,6 @@
+__config
+
+with snap as (select * from {{ ref("snap_dim_customer") }} where dbt_valid_to is null)
+select
+    customer_id, full_name_clean, email_norm, country_code, dbt_valid_from as valid_from
+from snap
